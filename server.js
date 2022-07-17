1a1049dc4f4641e7dd1ed55576fb50ac5c071b27
WojciechSwiderski <wojtek.swiderski8@gmail.com>
2022-07-17 22:02:28 +0200

    added compression

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+var compression = require('compression');
 const path = require('path');
 const app = express();
+app.use(compression())
 app.use(express.static(__dirname + '/dist/massby-candle-store'));
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname +
